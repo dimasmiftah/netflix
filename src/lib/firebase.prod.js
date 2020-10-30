64f58e59e3739916bb605a48c3ec946cb813a6fb
Dimas Miftahul Huda <dimas.mfth@gmail.com>
2020-10-30 17:26:52 +0700

    connected with firebase and created context for it

---
+++ src/lib/firebase.prod.js
@@ -0,0 +1,18 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import { seedDatabase } from '../seed';
+
+const config = {
+  apiKey: 'AIzaSyDcjRfUb76HGcQeM7pO4O9qqvaz1gccLps',
+  authDomain: 'netflix-78f38.firebaseapp.com',
+  databaseURL: 'https://netflix-78f38.firebaseio.com',
+  projectId: 'netflix-78f38',
+  storageBucket: 'netflix-78f38.appspot.com',
+  messagingSenderId: '680564733943',
+  appId: '1:680564733943:web:a7994425b08a35fc07e0f2',
+};
+
+const firebase = Firebase.initializeApp(config);
+
+export { firebase };
