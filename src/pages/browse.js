5756f5fc063a2061b6bd3b5963ac9abe9ed59e98
Dimas Miftahul Huda <dimas.mfth@gmail.com>
2020-10-31 22:14:52 +0700

    utility - selectionFilter

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useContent } from '../hooks';
+import selectionFilter from '../utils/selection-filter';
 
 export default function Browse() {
   // we need the series and the films
   const { series } = useContent('series');
   const { films } = useContent('films');
-  console.log(series);
-  console.log(films);
+
   // we need slides
   // pass it to the browse container
 
