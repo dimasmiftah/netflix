2183056031f2c0f02758147dee1651905fe2adc0
Dimas Miftahul Huda <dimas.mfth@gmail.com>
2020-11-02 00:28:45 +0700

    building header profile

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -2,6 +2,8 @@ import React, { useContext, useState, useEffect } from 'react';
 import { SelectProfileContainer } from './profiles';
 import { FirebaseContext } from '../context/firebase';
 import { Loading, Header } from '../components';
+import * as ROUTES from '../constants/routes';
+import logo from '../logo.svg';
 
 export function BrowseContainer({ slides }) {
   const [profile, setProfile] = useState({});
@@ -20,8 +22,24 @@ export function BrowseContainer({ slides }) {
     <>
       {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
 
-      <Header src='joker1'>
-        <p>hello</p>
+      <Header src='joker1' dontShowOnSmallViewPort>
+        <Header.Frame>
+          <Header.Logo to={ROUTES.HOME} src={logo} alt='Netflix' />
+          <Header.TextLink>Series</Header.TextLink>
+          <Header.TextLink>Films</Header.TextLink>
+        </Header.Frame>
+        <Header.Feature>
+          <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
+          <Header.Text>
+            Forever alone in a crowd, failed comedian Arthur Fleck seeks
+            connection as he walks the streets of Gotham City. Arthur wears two
+            masks -- the one he paints for his day job as a clown, and the guise
+            he projects in a futile attempt to feel like he's part of the world
+            around him. Isolated, bullied and disregarded by society, Fleck
+            begins a slow descent into madness as he transforms into the
+            criminal mastermind known as the Joker.
+          </Header.Text>
+        </Header.Feature>
       </Header>
     </>
   ) : (
